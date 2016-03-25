b371cacd4e80a93f0362a05c2f2ff9a44004fdaa
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-25 10:28:45 -0700

    Create NewMealComponent file

---
+++ app/new-meal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component ({
+  selector: 'new-meal',
+  template: `
+
+  `
+})
+
+export class NewMealComponent{
+  
+}

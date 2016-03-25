85314b9d09348fc958c1b4ef080a3c9301d77e73
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-25 08:36:59 -0700

    Create header and footer

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, EventEmitter } from 'angular2/core';
+import { Component } from 'angular2/core';
 
+ @Component({
+   selector: 'my-app',
+   template: `
+  <div class="page-header">
+    <h1>Meal Tracker</h1>
+  </div>
+  <div class="container">
+    <h2></h2>
+  </div>
+  <div class="page-footer">
+    <p>Copyright (c) 2016 | Yvonna Contreras | Epicodus | Portland, OR</p>
+  </div>
+   `
+ })
 
+ export class AppComponent {
 
-@Component({
-  selector: 'my-app',
-  directives: [TaskListComponent],
-  template: `
-  `
-})
+ }
--- app/done.pipe.ts
+++ app/done.pipe.ts
@@ -1,13 +1,13 @@
 //example of a Pipe
-import {Pipe, PipeTransform} from 'angular2/core';
-
-@Pipe({
-  name: "done",
-  pure: false
-})
-export class DonePipe implements PipeTransform {
-  transform(input: , args) {
-
-
-  }
-}
+// import {Pipe, PipeTransform} from 'angular2/core';
+//
+// @Pipe({
+//   name: "done",
+//   pure: false
+// })
+// export class DonePipe implements PipeTransform {
+//   transform(input: , args) {
+//
+//
+//   }
+// }

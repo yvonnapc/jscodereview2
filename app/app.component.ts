8f8117e9fddf9a95c0dfa35af067b1f3ac644916
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-25 09:15:05 -0700

    Create onMealSelect property in MealListComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
+  outputs: ['onMealSelect'],
   template: `
   <h3 *ngFor="#currentMeal of mealList" (click)="mealClicked(currentMeal)">
     {{ currentMeal.name }}
@@ -13,17 +14,24 @@ import { Component } from 'angular2/core';
 export class MealListComponent{
   public mealList: Meal[];
   mealClicked(clickedMeal: Meal): void {
-    console.log(clickedMeal);
+    console.log("child", clickedMeal);
+  }
+  public onMealSelect: EventEmitter<Meal>;
+  constructor(){
+    this.onMealSelect = new EventEmitter();
   }
 }
+
  @Component({
    selector: 'my-app',
+   directives: [MealListComponent],
    template: `
   <div class="page-header">
     <h1>Meal Tracker</h1>
   </div>
   <div class="container">
-    <h2 *ngFor="#meal of meals" (click)="mealWasSelected(meal)">{{ meal.name }}</h2>
+    <h2>Entries:</h2>
+    <meal-list [mealList]="meals"></meal-list>
   </div>
   <div class="page-footer">
     <p>Copyright (c) 2016 | Yvonna Contreras | Epicodus | Portland, OR</p>
@@ -41,7 +49,7 @@ export class MealListComponent{
      ];
    }
    mealWasSelected(clickedMeal: Meal): void{
-     console.log(clickedMeal);
+     console.log("parent", clickedMeal);
    }
  }
 

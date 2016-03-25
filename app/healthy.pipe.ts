5943907abfc2ab127f552dddb39feb8cdf59240b
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-25 11:04:57 -0700

    Create conditions for HealthyPipe

--- app/healthy.pipe.ts
+++ app/healthy.pipe.ts
@@ -7,8 +7,18 @@ import { Meal } from './meal.model';
 })
 
 export class HealthyPipe implements PipeTransform {
-  transform(input: , args) {
-
-
+  transform(input: Meal[] , args) {
+    var desiredHealthyState = args[0];
+    if(desiredHealthyState === "healthy"){
+      return input.filter((meal) => {
+        return meal.calories <= 300;
+      });
+    } else if (desiredHealthyState === "notHealthy"){
+      return input.filter((meal) => {
+        return meal.calories >= 301;
+      });
+    } else {
+      return input;
+    }
   }
 }
